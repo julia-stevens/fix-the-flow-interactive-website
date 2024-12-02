0b691cdbc2169680089e3c3a7382c166ecbee98d
julia-stevens <juliastevens02@gmail.com>
2024-12-02 17:44:58 +0100

    #4 input range & js: seek slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,13 @@
 let playPauseButton = document.querySelector(".playpause-track"); // play/pause knop
 let buttonImage = document.querySelector(".playpause-track img"); // play/pause img
 
+// let seekSlider = document.querySelector(".seek-slider");
+// let currentTime = document.querySelector(".current-time");
+// let totalDuration = document.querySelector(".total-duration");
+
 let trackIndex = 0; // begin bij eerste track (back to black)
 let isPlaying = false; // bij openen pagina niet afspelen
+let updateTimer; 
 
 let currentTrack = document.createElement("audio"); // nieuwe audio variabele
 
@@ -11,6 +16,58 @@ let trackList = [ // array met nummers
         path: "./assests/Back-To-Black_Amy-Winehouse.mp3" // path van eerste nummer
     }
 ]
+
+loadTrack(trackIndex); // laad trackIndex
+
+
+function loadTrack(trackIndex) {
+    // clear previous seek timer
+    // clearInterval(updateTimer); 
+    // resetValues();
+
+
+    // load new track
+    currentTrack.src = trackList[trackIndex].path; // source currentTrack = haal uit array (start bij 0 (dus eerste nummer) en neem het path)
+    currentTrack.load(); // laad currentTrack variabele
+
+    // updateTimer = setInterval(seekUpdate, 1000);
+}
+
+// function resetValues() {
+//     currentTime.textContent = "00:00";
+//     totalDuration.textContent = "00:00";
+//     seekSlider.value = 0;
+// }
+
+// function seekTo() {
+//     seekTo = currentTrack.duration * (seekSlider.value / 100);
+
+//     currentTrack.currentTime = seekTo; 
+// }
+
+// function seekUpdate() {
+//     let seekPosition = 0; 
+
+//     if (!isNaN(currentTrack.duration)) {
+//         seekPosition = currentTrack.currentTime * (100 / currentTrack.duration); 
+//         seekSlider.value = seekPosition;
+
+//         let currentMinutes = Math.floor(currentTrack.currentTime / 60);
+//         let currentSeconds = Math.floor(currentTrack.currentTime - currentMinutes * 60);
+//         let durationMinutes = Math.floor(currentTrack.duration / 60);
+//         let durationSeconds = Math.floor(currentTrack.duration - durationMinutes * 60);
+
+//         // Add a zero to the single digit time values
+//         if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+//         if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+//         if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+//         if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+    
+//         // Display the updated duration
+//         currentTime.textContent = currentMinutes + ":" + currentSeconds;
+//         totalDuration.textContent = durationMinutes + ":" + durationSeconds;
+//     }
+// }
 
 playPauseButton.addEventListener("click", playPauseTrack); // bij klik op knop, activeer functie playPauseTrack
 
@@ -29,9 +86,3 @@ function playPauseTrack() {
     }
 }
 
-function loadTrack(trackIndex) {
-    currentTrack.src = trackList[trackIndex].path; // source currentTrack = haal uit array (start bij 0 (dus eerste nummer) en neem het path)
-    currentTrack.load(); // laad currentTrack variabele
-}
-
-loadTrack(trackIndex); // laad trackIndex
